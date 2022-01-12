1d17e0e901b4237829c92a02e5b38e503c024a6f
Anatolii Butko <anatolii.butko93@gmail.com>
2022-01-12 13:46:38 +0200

    Description

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace parsing
+{
+    class Program
+    {
+        class Contact
+        {
+            public string Name { get; set; }
+            public string PhoneType { get; set; }
+            public string Address { get; set; }
+            public string NetWorth { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string checkname = Console.ReadLine();
+            XmlDocument xDoc = new XmlDocument();
+            const string Filename = @"C:\Users\anatolii.butko\source\repos\parsing\contacts.xml";
+            xDoc.Load(Filename);
+            List<Contact> contacts = new List<Contact>();
+            XmlElement xRoot = xDoc.DocumentElement;
+            if (xRoot != null && xRoot.HasChildNodes)
+            {
+                foreach (XmlElement xnode in xRoot)
+                {
+                    Contact contact = new Contact();
+                    if (xnode.Name == "Name")
+                    {
+                        contact.Name = xnode.InnerText;
+                    }
+
+                    if (xnode.Name == "Phone Type")
+                    {
+                        foreach (XmlNode childnode in xnode.ChildNodes)
+                        {
+                            XmlNode attr = childnode.Attributes.GetNamedItem("Phone Type");
+                            contact.PhoneType = attr?.Value;
+                            contacts.Add(contact);
+                        }
+                    }
+
+                    if (xnode.Name == "Address")
+                    {
+                        contact.Address = xnode.InnerText;
+                    }
+
+                    if (xnode.Name == "NetWorth")
+                    {
+                        contact.NetWorth = xnode.InnerText;
+                    }
+                        
+                    contacts.Add(contact);
+                }
+
+                foreach (Contact c in contacts)
+                {
+                    if (checkname == c.Name)
+                    {
+                        Console.WriteLine($"{c.Name} - {c.PhoneType} - {c.Address} - {c.NetWorth}");
+                    }   
+                }  
+            }
+        }
+    }
+}
---
+++ contacts.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Serialization;
+
+public class Contacts 
+{   
+    public Contact[] Items { get; set; }
+}
+
+public class Contact 
+{
+    private Phone[] phoneField;
+    
+    private Address[] addressField;
+    
+    public string Name { get; set; }
+       
+    public string NetWorth { get; set; }
+      
+    public Phone[] Phone { get; set; }
+        
+    public class Address[] Address { get; set; }
+}
+
+public class Phone 
+{
+    public string Type { get; set; }
+    
+    public string Value { get; set; }
+}
+
+public class CAddress 
+{
+    public string Street1 { get; set; }
+     
+    public string City { get; set; }
+    
+    public string State { get; set; }
+    
+    public string Postal { get; set; }
+}
