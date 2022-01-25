5595aab1825d956e41e308bcaf1d5b04b3abac8b
Anatolii Butko <anatolii.butko93@gmail.com>
2022-01-25 12:52:21 +0200

    Minor fixes.

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -37,7 +37,6 @@
             string output = "Incorrect data! Please check your input and try again.";
             foreach (XElement C in xdoc.Element("Contacts").Elements("Contact"))
             {
-                if (C == null) return;
                 string name = C.Element("Name").Value;                
                 string firstname = name.Substring(0, name.IndexOf(' '));                
                 string lastname = name.Substring(name.IndexOf(' ') + 1, name.Length - name.IndexOf(' ') - 1);                
@@ -45,32 +44,29 @@
                 {
                     temp.Add("Name: " + name);
                     string temp1 = "";
-                    temp1 += ", " + C.Element("Phone").Attribute("Type").Value + " " + C.Element("Phone").Value;
-                    temp1 = temp1.TrimStart(',');
-                    temp1 = temp1.TrimStart(' ');                    
-                    temp.Add("Phones: " + temp1);
-                    
-                    foreach (XElement A in xdoc.Element("Contacts").Element("Contact").Elements("Address"))
+                    if (C.Element("Phone") != null)                    
                     {
-                        string temp2 = "";                        
-                        temp2 += ", " + A.Element("Street1").Value;
-                        temp2 += ", " + A.Element("City").Value;
-                        temp2 += ", " + A.Element("State").Value;
-                        temp2 += ", " + A.Element("Postal").Value;
-                        temp2 = temp2.TrimStart(',');
-                        temp2 = temp2.TrimStart(' ');
-                        Console.WriteLine(temp2);
-                        temp.Add("Address: " + temp2);                        
+                        temp1 += ", " + C.Element("Phone").Attribute("Type").Value + " " + C.Element("Phone").Value;
+                        Console.WriteLine(temp1);
                     }
+                    temp1 = temp1.TrimStart(',');
+                    temp1 = temp1.TrimStart(' ');
+                    temp.Add("Phones: " + temp1);
+                    string temp2 = "";
+                    foreach (var childElem2 in C.Element("Address").Elements())
+                    {
+                        temp2 += ", " + childElem2.Name + " " + childElem2.Value;                        
+                    }
+                    temp2 = temp2.TrimStart(',');
+                    temp2 = temp2.TrimStart(' ');
+                    temp.Add("Address: " + temp2);
                     if (C.Element("NetWorth").Value == "None")
                     {
-                        temp.Add("NetWorth: There is no salary increase");
-                        Console.WriteLine("NetWorth: There is no salary increase");
+                        temp.Add("NetWorth: There is no salary increase");                        
                     }
                     else
                     {
-                        temp.Add("NetWorth: " + C.Element("NetWorth").Value);
-                        Console.WriteLine("NetWorth: " + C.Element("NetWorth").Value);
+                        temp.Add("NetWorth: " + C.Element("NetWorth").Value);                        
                     }
                     output = "";
                     foreach (string t in temp) output += "\n" + t;
