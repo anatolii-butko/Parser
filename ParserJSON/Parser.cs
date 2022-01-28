8af9e1d2fe7da6f80e743cfc8a7170fd1745eccd
Anatolii Butko <anatolii.butko93@gmail.com>
2022-01-28 15:14:02 +0200

    Minor fixes

--- ParserJSON/Parser.cs
+++ ParserJSON/Parser.cs
@@ -1,5 +1,4 @@
-﻿
-namespace ParserJson
+﻿namespace ParserJson
 {
 
     #region Using
@@ -11,9 +10,51 @@ namespace ParserJson
     using Newtonsoft.Json.Linq;
     using System.Runtime.Serialization.Json;
     using Newtonsoft.Json;
-
+    using Newtonsoft.Json.Schema;
 
     #endregion
+
+    /// <summary>
+    /// Class which need for deserialization
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Interface Settings")]
+        public string InterfaceSettings { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Media Interface Settings")]
+        public string MediaInterfaceSettings { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Port Settings")]
+        public string PortSettings { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Unique ID")]
+        public string UniqueId { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("MAC Address")]
+        public string MacAddress { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Component Interconnect ID")]
+        public string ComponentInterconnectId { get; set; }
+    }
 
     /// <summary>
     /// The class which contains json parser.
@@ -39,20 +80,25 @@ namespace ParserJson
             string output = "Incorrect data! Please check your input and try again.";
             Console.WriteLine("Enter the data you want to find in the file: ");
             string check = Console.ReadLine();
-            JObject jobject = JObject.Parse(File.ReadAllText(path));
+            string json = File.ReadAllText(path);            
+            JObject jobject = JObject.Parse(json);
+            var data = JsonConvert.DeserializeObject<Data>(json);
+            Console.WriteLine(data.UniqueId);
             do
             {
                 if (jobject.ContainsKey(check))
                 {
                     output = jobject[check].ToString();
-                }                
+                }
                 Console.WriteLine(output);
                 Console.WriteLine("Enter the data you want to find in the file: ");
                 check = Console.ReadLine();
-            } while (output != null);              
-        }
+            } while (output != null);
+        }       
 
         #endregion
 
     }
+    
+
 }
